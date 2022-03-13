bba14b18bbaec317dcd1e5794afa60ca74dab139
Szymon Budziak <75530523+Szymon-Budziak@users.noreply.github.com>
2022-03-13 18:38:44 +0100

    Deleted cmake-build-debug

--- Exercises_1/tests/cmake-build-debug/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/szymon/Studia_AGH/Semestr_4/C++/Cpp_exercises_solutions/Exercises_1/tests/lib/googlemock
-# Build directory: /home/szymon/Studia_AGH/Semestr_4/C++/Cpp_exercises_solutions/Exercises_1/tests/cmake-build-debug/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- Exercises_1/tests/cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/szymon/Studia_AGH/Semestr_4/C++/Cpp_exercises_solutions/Exercises_1/tests/lib/googletest
-# Build directory: /home/szymon/Studia_AGH/Semestr_4/C++/Cpp_exercises_solutions/Exercises_1/tests/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
